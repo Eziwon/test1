04a8ffbe7b9142d3f2c03f104af7a55f8210741b
JiwonLeeeeeeee <eziwon0228@gmail.com>
2022-07-27 03:31:04 +0900

    using namespace 부가 주석

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-//using namespace std;
+//using namespace std; --> 이게 있으면 cout와 endl 앞에 std::을 붙이지 않아도 됨.
 
 int main (int argc, char **argv) {
-    std::cout << "Hello World!\n" << std::endl;
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
