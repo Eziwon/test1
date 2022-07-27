9da287d5d6516335aa3fcfbbeeb99957037ffc75
JiwonLeeeeeeee <eziwon0228@gmail.com>
2022-07-27 22:31:14 +0900

    Create cinout.cpp

---
+++ cinout.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string user_name;
+    cout << "이름을 입력하세요." << endl;
+    cin >> user_name;
+    cout << "입력된 이름은 " << user_name << "입니다." << endl;
+}
