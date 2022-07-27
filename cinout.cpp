df6209abac626776cb2ea3c292ea9452fc7b90b1
JiwonLeeeeeeee <eziwon0228@gmail.com>
2022-07-27 23:03:01 +0900

    새로운 예시(덧셈) 추가

--- cinout.cpp
+++ cinout.cpp
@@ -3,9 +3,17 @@
 using namespace std;
 
 int main() {
+    //1. 이름 입력받기
     string user_name;
     cout << "이름을 입력하세요." << endl;
     cin >> user_name;
-    cout << "입력된 이름은 " << user_name << "입니다." << endl;
+    cout << "입력된 이름은 " << user_name << "입니다.\n" << endl;
+
+    //2. 두개의 숫자를 입력받은 후, 두 숫자의 합을 출력
+    int i, j;
+    cout << "Give two numbers" <<endl;
+    cin >> i >> j;
+    cout << "Sum = " << i+j << "\n";
+
     return 0;
 }
