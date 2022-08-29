b1d44d77d67f566261f750509f3fa8f99316ea24
JiwonLeeeeeeee <eziwon0228@gmail.com>
2022-08-29 17:10:06 +0900

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-
-//using namespace std; --> 이게 있으면 cout와 endl 앞에 std::을 붙이지 않아도 됨.
-
-int main (int argc, char **argv) {
-    std::cout << "Hello World!" << std::endl;
+using namespace std;
+int main() {
+    cout << "Hello, World!\n";
     return 0;
 }
